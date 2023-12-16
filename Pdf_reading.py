bc4566ea137a39bde93e41d4b8802149e691ce4d
MiroshG <146867896+MiroshG@users.noreply.github.com>
2023-12-16 15:42:20 +0100

    Add files via upload

--- Pdf_reading.py
+++ Pdf_reading.py
@@ -12,37 +12,39 @@ conn = mysql.connector.connect(
     database='boxing_data'
 )
 
+cursor = conn.cursor()
+
 # DICTIONARIES ##
 
 dictionary_persona={"name" :"",
                     "nationality":"",
-                    "date birth":"",
+                    "date_birth":"",
                     "weight":"",
                     "gender":"",
                     "role":""}
 
 dictionary_match={
-                "Name red boxer":"",
-                "Name blue boxer":"",
-                "Referee":"",
-                "Judge 1":"",
-                "Judge 2":"",
-                "Judge 3":"",
-                "Judge 4":"",
-                "Judge 5":"",
-                "Winner":"",
-                "Result":"",
-                "Decision":""}
+                "id_boxer_red":"",
+                "id_boxer_blue":"",
+                "id_referee":"",
+                "id_judge_1":"",
+                "id_judge_2":"",
+                "id_judge_3":"",
+                "id_judge_4":"",
+                "id_judge_5":"",
+                "id_winner":"",
+                "result":"",
+                "decision":""}
 
 dictionary_points={
-                "Judge":"",
-                "Name red boxer":"",
-                "Name blue boxer":"",
-                "Boxer name points":"",
-                "Round 1":"",
-                "Round 2":"",
-                "Round 3":"",
-                "Total":""}
+                "id_judge":"",
+                "name_boxer_red":"",
+                "name_boxer_blue":"",
+                "id_boxer":"",
+                "points_1":"",
+                "points_2":"",
+                "points_3":"",
+                "total":""}
 
 # LIST OF THE DICTIONARIES
 
@@ -55,37 +57,38 @@ list_points=[]
 def fill_dictionary_persona(name, nationality, date_birth, weight, gender, role, dictionary):
     dictionary["name"]=name
     dictionary["nationality"]=nationality
-    dictionary["date birth"]=date_birth
+    dictionary["birth_date"]=date_birth
     dictionary["weight"]=weight
     dictionary["gender"]=gender
     dictionary["role"]=role
 
-    list_persona.append(dictionary.copy())
+    if dictionary not in list_persona:
+        list_persona.append(dictionary.copy())
 
 def fill_dictionary_match(red_boxer, blue_boxer, referee, judge_1, judge_2, judge_3, judge_4, judge_5, winner_match, result, decision, dictionary):
-    dictionary["Name red boxer"]=red_boxer
-    dictionary["Name blue boxer"]=blue_boxer       
-    dictionary["Referee"]=referee       
-    dictionary["Judge 1"]=judge_1     
-    dictionary["Judge 2"]=judge_2      
-    dictionary["Judge 3"]=judge_3      
-    dictionary["Judge 4"]=judge_4    
-    dictionary["Judge 5"]=judge_5     
-    dictionary["Winner"]=winner_match
-    dictionary["Result"]=result
-    dictionary["Decision"]=decision
+    dictionary["id_boxer_red"]=red_boxer
+    dictionary["id_boxer_blue"]=blue_boxer       
+    dictionary["id_referee"]=referee       
+    dictionary["id_judge_1"]=judge_1     
+    dictionary["id_judge_2"]=judge_2      
+    dictionary["id_judge_3"]=judge_3      
+    dictionary["id_judge_4"]=judge_4    
+    dictionary["id_judge_5"]=judge_5     
+    dictionary["id_winner"]=winner_match
+    dictionary["result"]=result
+    dictionary["decision"]=decision
 
     list_match.append(dictionary.copy())      
 
 def fill_dictionary_points(judge, red_boxer, blue_boxer, boxer_name_points, round_1, round_2, round_3, total, dictionary):
-    dictionary["Judge"]=judge
-    dictionary["Name red boxer"]=red_boxer
-    dictionary["Name blue boxer"]=blue_boxer
-    dictionary["Boxer name points"]=boxer_name_points
-    dictionary["Round 1"]=round_1
-    dictionary["Round 2"]=round_2
-    dictionary["Round 3"]=round_3
-    dictionary["Total"]=total
+    dictionary["id_judge"]=judge
+    dictionary["name_boxer_red"]=red_boxer
+    dictionary["name_boxer_blue"]=blue_boxer
+    dictionary["id_boxer"]=boxer_name_points
+    dictionary["points_1"]=round_1
+    dictionary["points_2"]=round_2
+    dictionary["points_3"]=round_3
+    dictionary["total"]=total
 
     list_points.append(dictionary.copy())
 
@@ -95,55 +98,108 @@ def fill_dictionary_points(judge, red_boxer, blue_boxer, boxer_name_points, roun
 def points_judge(judge_index,tables):
     
     # JUST 1 ROUND #
-    if len(tables[0])== 5: 
-        round_1=0
-        round_2=0
-        round_3=0
-        total=0
 
-    # 2 ROUNDS #
-    elif len(tables[0])== 5+1: 
-        round_1=tables[0].iloc[3, judge_index+1]
-        round_2=tables[0].iloc[4, judge_index+1]
-        round_3=0
-        total=tables[0].iloc[5, judge_index+1]
+    try:
+        if len(tables[0])== 5: 
+            round_1=0
+            round_2=0
+            round_3=0
+            total=0
 
-    # 3 ROUNDS #
-    elif len(tables[0])== 5+2: 
-        round_1=tables[0].iloc[3, judge_index+1]
-        round_2=tables[0].iloc[4, judge_index+1]
-        round_3=tables[0].iloc[5, judge_index+1]
-        total=tables[0].iloc[6, judge_index+1]
+        # 2 ROUNDS #
+        elif len(tables[0])== 5+1: 
+            round_1=tables[0].iloc[3, judge_index+1]
+            round_2=tables[0].iloc[4, judge_index+1]
+            round_3=0
+            total=tables[0].iloc[5, judge_index+1]
 
+        # 3 ROUNDS #
+        elif len(tables[0])== 5+2: 
+            round_1=tables[0].iloc[3, judge_index+1]
+            round_2=tables[0].iloc[4, judge_index+1]
+            round_3=tables[0].iloc[5, judge_index+1]
+            total=tables[0].iloc[6, judge_index+1]
 
-    if isinstance(round_1, str):
-        round_1_red , round_1_blue = round_1.split(" ")
-    else:
-        round_1_red , round_1_blue ="",""
+        try:
+            if isinstance(round_1, str):
+                round_1_red , round_1_blue = round_1.split(" ")
+                round_1_red=int(round_1_red)
+                round_1_blue=int(round_1_blue)
 
-    if isinstance(round_2, str):
-        round_2_red , round_2_blue = round_2.split(" ")
-    else:
-        round_2_red , round_2_blue ="",""
+            else:
+                round_1_red , round_1_blue =None, None
 
-    if isinstance(round_3, str):
-        round_3_red , round_3_blue = round_3.split(" ")
-    else:
-        round_3_red , round_3_blue ="",""
+            if isinstance(round_2, str):
+                round_2_red , round_2_blue = round_2.split(" ")
+                round_2_red=int(round_2_red)
+                round_2_blue=int(round_2_blue)
+            else:
+                round_2_red , round_2_blue =None, None
 
-    if isinstance(total, str):
-        total_red , total_blue = total.split(" ")
-    else:
-        total_red , total_blue ="",""
+            if isinstance(round_3, str):
+                round_3_red , round_3_blue = round_3.split(" ")
+                round_3_red=int(round_3_red)
+                round_3_blue=int(round_3_blue)
+            else:
+                round_3_red , round_3_blue =None, None
 
-    return(round_1_red , round_1_blue, round_2_red , round_2_blue, round_3_red , round_3_blue, total_red , total_blue)
+            if isinstance(total, str):
+                total_red , total_blue = total.split(" ")
+                total_red=int(total_red)
+                total_blue=int(total_blue)
+            else:
+                total_red , total_blue =None, None
+        
+            return(round_1_red , round_1_blue, round_2_red , round_2_blue, round_3_red , round_3_blue, total_red , total_blue)
+        except UnboundLocalError:
+            return(None, None, None, None, None, None, None,None)
+    except IndexError:
+        return(None, None, None, None, None, None, None,None)
+
 
 def birth_year(boxer,text):
-    match=re.search(rf"{re.escape(boxer)}\n(.+?)\n", text)
-    date=match.group(1)
+    surname=boxer.split(" ")[0]
+    name=boxer.split(" ")[1]
+    find_boxer=surname+" "+name
+    if name.isupper():
+        find_boxer=surname
+    match=re.search(rf"{re.escape(find_boxer)}\s+(.*?)\n(.+?)\n", text)
+    number_of_names=boxer.count(' ')
+    if number_of_names>=2:
+        date=match.group(2)
+    else:
+        date=match.group(1)
     year=date.split(" ")[-1]
-    return(year)
+    return(int(year))
 
+def weight_092(boxer,text):
+    boxer_weight=re.search(rf"{re.escape(boxer)}\n(.+?)\n(.+?)\n", text)
+    weight=boxer_weight.group(1)
+    kg=weight.split('\\')[1]
+    return(float(kg))
+
+def open_and_read_file_Entry_list(file_path):
+    try:
+        pdf_document = fitz.open(file_path)
+        
+        
+        # Process the PDF (e.g., extract text)
+        for page_num in range(pdf_document.page_count):
+
+            page = pdf_document.load_page(page_num)
+            text_dates = page.get_text("text")
+            
+
+            # REGULAR EXPRESSIONS IN THE FOLLOWING LINES: ################
+
+        pdf_document.close()  # Close the PDF file
+        return(text_dates)
+
+    except Exception as e:
+
+        print(f"Error processing {file_path}: {e}")
+    
+    
 
     
 
@@ -154,12 +210,10 @@ def birth_year(boxer,text):
 ############################################# LOOP TO RUN OVER ALL THE PDFS ##############################
 
 pdf_start='C:\\Users\\Mirosh\\Desktop\\Data\\'
-table_birth_year=""
-tables=""
 
 # List of weights to navigate through
-#gender_boxers = ['H', 'M']
-gender_boxers=['H2']
+gender_boxers = ['M', 'H']
+#gender_boxers=['H2']
 
 for gender in gender_boxers:
 
@@ -177,25 +231,17 @@ for gender in gender_boxers:
 
                 file_path = os.path.join(weight_path, boxing_phase, "Entry_list.pdf")
 
-                try:
-                    pdf_document = fitz.open(file_path)
+                text_dates=open_and_read_file_Entry_list(file_path)
+
+
+                if weight=='092':
                     
-                    # Process the PDF (e.g., extract text)
-                    for page_num in range(pdf_document.page_count):
+                    file_path_2 = os.path.join(weight_path, boxing_phase, "Entry_list_weight.pdf")
 
-                        page = pdf_document.load_page(page_num)
-                        text_dates = page.get_text("text")
-                        lines=text_dates.split('\n')  # necessary to obtain some of the text
-                        
-                        #print(f"Text from {number_of_match} - Page {page_num + 1}:{text_dates}")
+                    text_weight=open_and_read_file_Entry_list(file_path_2)
 
-                        # REGULAR EXPRESSIONS IN THE FOLLOWING LINES: ################
 
-                    pdf_document.close()  # Close the PDF file
-
-                except Exception as e:
-
-                    print(f"Error processing {number_of_match}: {e}")
+                
 
             else:
                 
@@ -235,7 +281,15 @@ for gender in gender_boxers:
                             
 
                             gender_text= re.search(r"(MEN|WOMEN)'S", text)
-                            weight_text= re.search(r"(\d+)kg", text)
+                            if weight=='092':
+                                weight_text=re.search()
+                                weight_of_boxer_blue=weight_092(blue_boxer,text_weight)
+                                weight_of_boxer_red=weight_092(red_boxer,text_weight)
+                                weight_list=[weight_of_boxer_red, weight_of_boxer_blue, None, None, None, None, None, None]
+                            else:
+                                weight_text= re.search(r"(\d+)kg", text)
+                                weight_of_boxer=weight_text.group(1)
+                                weight_list=[float(weight_of_boxer), float(weight_of_boxer), None, None, None, None, None, None]
                             phase= lines[2].strip()
 
                             red_boxer= lines[19].strip()
@@ -272,10 +326,7 @@ for gender in gender_boxers:
                             judge_5_text= re.search(r"Judge 5:\n(.+?)\n", text)
                             nationality_judge_5 = lines[35].strip()
 
-                            """DUDAS"""
-
                             gender=gender_text.group(1) 
-                            weight=weight_text.group(1)
                             referee=referee_text.group(1)
                             referee_nationality=referee_nationality_text.group(1)
                             judge_1=judge_1_text.group(1)
@@ -296,11 +347,11 @@ for gender in gender_boxers:
                     name_list=[red_boxer, blue_boxer, referee, judge_1, judge_2, judge_3, judge_4, judge_5]
                     nationality_list=[red_boxer_nationality, blue_boxer_nationality, referee_nationality, nationality_judge_1, nationality_judge_2, nationality_judge_3, nationality_judge_4, nationality_judge_5]
 
+                    print(file_path)
                     red_boxer_year=birth_year(red_boxer, text_dates)
                     blue_boxer_year=birth_year(blue_boxer, text_dates)
 
-                    date_birth_list=[red_boxer_year, blue_boxer_year, "", "", "", "", "", ""]
-                    weight_list=[weight, weight, "", "", "", "", "", ""]
+                    date_birth_list=[red_boxer_year, blue_boxer_year, None, None, None, None, None, None]
                     gender_list=[gender, gender, "", "", "", "", "", ""]
                     role_list=["boxer", "boxer", "referee", "judge", "judge", "judge", "judge", "judge"]
 
@@ -323,8 +374,101 @@ for gender in gender_boxers:
 
                         fill_dictionary_points(judges_list[i], red_boxer, blue_boxer, blue_boxer, round_1_blue, round_2_blue, round_3_blue, total_blue, dictionary_points) # blue boxer
 
-            
-print(list_match)
 
 
 
+cursor.executemany("""
+    INSERT INTO persona (name,birth_date,weight,gender,role)
+    VALUES (%(name)s, %(birth_date)s, %(weight)s, %(gender)s, %(role)s)""", list_persona)
+conn.commit()       
+
+list_names=[]
+for dictionary in list_match:
+    list_names.append(dictionary["id_boxer_red"])
+    list_names.append(dictionary["id_boxer_blue"])
+    list_names.append(dictionary["id_referee"])
+    list_names.append(dictionary["id_judge_1"])
+    list_names.append(dictionary["id_judge_2"])
+    list_names.append(dictionary["id_judge_3"])
+    list_names.append(dictionary["id_judge_4"])
+    list_names.append(dictionary["id_judge_5"])
+
+parsed_names = ",".join(list_names)  #parsed_names="name1,name2,name3,..."
+format_strings = ','.join(['%s'] * len(list_names)) #format_string="%s %s,%s,...""
+
+query_names = "select id, name from persona where name in( " + format_strings+")" # query to obtain the list of names
+cursor.execute(query_names,list_names) 
+
+result_names=cursor.fetchall() # gives me id and name
+
+for dictionary in list_match:        
+    name_boxer_red = dictionary["id_boxer_red"] # get the name 
+    id_boxer_red =[row[0] for row in result_names if row[1] == name_boxer_red][0] # compare the name with the list 
+    dictionary["id_boxer_red"]=id_boxer_red                                 # result_names and takes the id corresponding 
+    name_boxer_blue = dictionary["id_boxer_blue"]                           # to that name
+    id_boxer_blue =[row[0] for row in result_names if row[1] == name_boxer_blue][0]
+    dictionary["id_boxer_blue"]=id_boxer_blue
+    name_referee = dictionary["id_referee"]
+    id_referee =[row[0] for row in result_names if row[1] == name_referee][0]
+    dictionary["id_referee"]=id_referee
+    name_judge_1 = dictionary["id_judge_1"]
+    id_judge_1 =[row[0] for row in result_names if row[1] == name_judge_1][0]
+    dictionary["id_judge_1"]=id_judge_1
+    name_judge_2 = dictionary["id_judge_2"]
+    id_judge_2 =[row[0] for row in result_names if row[1] == name_judge_2][0]
+    dictionary["id_judge_2"]=id_judge_2
+    name_judge_3 = dictionary["id_judge_3"]
+    id_judge_3 =[row[0] for row in result_names if row[1] == name_judge_3][0]
+    dictionary["id_judge_3"]=id_judge_3
+    name_judge_4 = dictionary["id_judge_4"]
+    id_judge_4 =[row[0] for row in result_names if row[1] == name_judge_4][0]
+    dictionary["id_judge_4"]=id_judge_4
+    name_judge_5 = dictionary["id_judge_5"]
+    id_judge_5 =[row[0] for row in result_names if row[1] == name_judge_5][0]
+    dictionary["id_judge_5"]=id_judge_5
+    name_winner = dictionary["id_winner"]
+    id_winner =[row[0] for row in result_names if row[1] == name_winner][0]
+    dictionary["id_winner"]=id_winner
+
+cursor.executemany("""
+    INSERT INTO matches (id_boxer_red,id_boxer_blue,id_referee,id_judge_1,id_judge_2,id_judge_3,id_judge_4,id_judge_5,id_winner,result,decision)
+    VALUES (%(id_boxer_red)s, %(id_boxer_blue)s, 
+    %(id_referee)s, %(id_judge_1)s, %(id_judge_2)s,
+    %(id_judge_3)s, %(id_judge_4)s, 
+    %(id_judge_5)s, %(id_winner)s, %(result)s, %(decision)s)""", list_match)
+conn.commit()       
+
+
+list_name_ids = [row[0] for row in result_names]
+format_strings = ','.join(['%s'] * len(result_names)) #format_string="%s %s,%s,...""
+
+# query to obtain the list of matches
+query_matches = "select id, id_boxer_red, id_boxer_blue from matches where id_boxer_red in( " + format_strings+") or id_boxer_blue in ( " + format_strings+")" 
+cursor.execute(query_matches,list_name_ids+ list_name_ids) # list_names_ids twice to give the names for red and for blue
+
+result_matches=cursor.fetchall() # gives me id of the match, id boxer red, id boxer blue
+
+for dictionary in list_points:        
+    name_boxer_red = dictionary["name_boxer_red"]
+    id_boxer_red = [row[0] for row in result_names if row[1] == name_boxer_red][0]
+    name_boxer_blue = dictionary["name_boxer_blue"]
+    id_boxer_blue = [row[0] for row in result_names if row[1] == name_boxer_blue][0]
+    id_match=[row[0] for row in result_matches if row[1] == id_boxer_red and row[2] == id_boxer_blue][0]
+    dictionary["id_match"]=id_match # add new key
+    dictionary.pop("name_boxer_red")  # remove the old ones
+    dictionary.pop("name_boxer_blue")
+    name_boxer = dictionary["id_boxer"]
+    id_boxer =[row[0] for row in result_names if row[1] == name_boxer][0]
+    dictionary["id_boxer"]=id_boxer
+    name_judge = dictionary["id_judge"]
+    id_judge =[row[0] for row in result_names if row[1] == name_judge][0]
+    dictionary["id_judge"]=id_judge
+
+
+cursor.executemany("""
+    INSERT INTO points (id_judge,id_match,id_boxer,points_1,points_2,points_3,total)
+    VALUES (%(id_judge)s, %(id_match)s,%(id_boxer)s, %(points_1)s, %(points_2)s, %(points_3)s, %(total)s)""", list_points)
+conn.commit()  
+
+cursor.close()
+conn.close()
